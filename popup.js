184128c6adba48ad83286ed36b4b9470eb3fe135
RyanWittibschlager <ryanpaul271828@gmail.com>
2015-03-08 17:11:34 -0400

    yeah... I should have committed awhile ago

--- popup.js
+++ popup.js
@@ -1,14 +1,9 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Author: Ryan Wittibschlager
+// License: ...there is none? Do whatever you want with this code.
 
-// Store CSS data in the "local" storage area.
-//
-// See note in options.js for rationale on why not to use "sync".
-var storage = chrome.storage.local;
+
 // clear it for now
-//storage.clear();
-var message = document.querySelector('#message');
+//chrome.storage.local.clear();
 
 var currentUrl = "";
 chrome.tabs.query({active:true,currentWindow:true},function(tabArray){
@@ -17,10 +12,10 @@ chrome.tabs.query({active:true,currentWindow:true},function(tabArray){
 });
 
 // Check if there is CSS specified.
-storage.get('dumpConfig', function(items) {
+chrome.storage.local.get('dumpConfig', function(items) {
   console.log("items: " + items);
   // If there is CSS specified, inject it into the page.
-  if (items.dumpConfig) {
+  if (items.dumpConfig && items.dumpConfig.length > 0) {
     var dumpConfig = items.dumpConfig;
     for (var i=0; i<dumpConfig.length; i++) {
       if (dumpConfig[i].active) {
@@ -33,14 +28,16 @@ storage.get('dumpConfig', function(items) {
     }
   } else {
     var optionsUrl = chrome.extension.getURL('options.html');
-    message.innerHTML = 'Add dumps in the <a target="_blank" href="' +
+    document.querySelector('#message').innerHTML = 'Add dumps in the <a target="_blank" href="' +
         optionsUrl + '">options page</a> first.';
   }
 });
 
 var createDumpURL = function (url, appended, substring) {
     // strip everything after the slot number
-    var dumpURL = url.substring(0, url.indexOf("/", url.toLowerCase().indexOf(substring.toLowerCase())));
+    var dumpURL = substring.length > 0
+                ? url.substring(0, url.indexOf("/", url.toLowerCase().indexOf(substring.toLowerCase())))
+                : url;
 
     // append the dump url
     dumpURL += "/" + appended;
